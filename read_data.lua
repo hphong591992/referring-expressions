968607974baf0431db4e728a81e57e37d091f2a7
Phong <hphong591992@gmail.com>
2016-12-02 03:43:45 -0500

    Add some TODOs

--- read_data.lua
+++ read_data.lua
@@ -3,4 +3,5 @@ local cjson = require 'cjson'
 -- TODO(hphong): Make the path become a parameter
 local file = io.open('/home/phong/Data/annotations/instances_val2014.json')
 
+-- TODO(hphong): Read and convert the json file to t7
 local table = cjson.decode(file:read('*'))
