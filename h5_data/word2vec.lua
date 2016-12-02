7c47fe55075f92dcb85f574dd758cb9c819345af
asrata <nju.angli@gmail.com>
2016-12-02 13:58:40 -0500

    Fix bug in saving wordvectors for Gref/UNCref dictionaries

--- h5_data/word2vec.lua
+++ h5_data/word2vec.lua
@@ -1,20 +1,35 @@
-io.input("UNC_RefExp_vocabulary.txt")
 
+local file = io.open("UNC_RefExp_vocabulary.txt", "r")
 local w2vutils = require('w2vutils')
+local tds = require 'tds'
 
-local dictionary = {}
+local idx2word, word2idx = tds.Vec(), tds.Hash()
 
+local cnt = 0
 while true do
-  local line = io.read()
+   local line = file:read()
 
-  if (line == nil) then
-    break
-  end
+   if not line then
+      break
+   end
 
-  local vector = w2vutils:word2vec(line)
-  dictionary[line] = vector
+   cnt = cnt + 1
+   idx2word[cnt] = line
+   word2idx[line] = cnt
+end
+file:close()
+local w2vsample = w2vutils:word2vec('a')
+local w2vdim = w2vsample:nElement()
+local w2vtensor = torch.Tensor(cnt, w2vdim)
+
+for i = 1, #idx2word do
+   local word = idx2word[i]
+   local vector = w2vutils:word2vec(word)
+   w2vtensor[i]:copy(vector)
 end
 
-torch.save('UNC_RefExp_vocab_vector.t7', dictionary, 'ascii')
-
-
+torch.save('UNC_RefExp_vocab_vector.t7', {
+   _idx2word = idx2word,
+   _word2idx = word2idx,
+   wordvec = w2vtensor
+})
